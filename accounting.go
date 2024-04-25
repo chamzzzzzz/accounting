d75093359e3c7368682f1ef86c333d75f2c2cfa2
Liu Chen <chamzzzzzz@gmail.com>
2024-04-25 13:03:17 +0800

    restart

--- accounting.go
+++ accounting.go
@@ -1,30 +1,34 @@
 package accounting
 
 type Account struct {
-	ID          string `json:"id"`
-	Type        string `json:"type"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	ID          string `json:"id,omitempty"`
+	Type        string `json:"type,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
 }
 
 type Entry struct {
-	Account  string `json:"account"`
-	Amount   string `json:"amount"`
-	Currency string `json:"currency"`
+	Account  string `json:"account,omitempty"`
+	Amount   string `json:"amount,omitempty"`
+	Currency string `json:"currency,omitempty"`
+	Note     string `json:"note,omitempty"`
 }
 
 type Voucher struct {
-	Date string   `json:"date"`
-	From []*Entry `json:"from"`
-	To   []*Entry `json:"to"`
+	ID          string   `json:"id,omitempty"`
+	Date        string   `json:"date,omitempty"`
+	Entries     []*Entry `json:"entries,omitempty"`
+	Description string   `json:"description,omitempty"`
+	Note        string   `json:"note,omitempty"`
+	Tags        []string `json:"tags,omitempty"`
 }
 
 type Journal struct {
-	Date     string     `json:"date"`
-	Vouchers []*Voucher `json:"vouchers"`
+	Date     string     `json:"date,omitempty"`
+	Vouchers []*Voucher `json:"vouchers,omitempty"`
 }
 
 type Book struct {
-	Accounts []*Account `json:"accounts"`
-	Journals []*Journal `json:"journals"`
+	Accounts []*Account `json:"accounts,omitempty"`
+	Journals []*Journal `json:"journals,omitempty"`
 }
